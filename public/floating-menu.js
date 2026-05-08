886decf74872595312fff929d333167ede115179
primo_it <primo_it@primo-itdeiMac.local>
2026-05-08 15:43:09 +0800

    add openMultUrl function

--- public/floating-menu.js
+++ public/floating-menu.js
@@ -15,6 +15,7 @@
     { label: '文本比對0', url: '../regexDomain/textDifferent.html' },
     { label: 'regex搜索文本', url: '../regexDomain/searchRegex.html' },
     { label: '剪貼板', url: '../regexDomain/clipboard_local_storage.html' },
+    { label: 'url多開', url: '../regexDomain/openUrls.html' },
     { label: '轉置文本', url: 'https://lzltool.com/Tools/TextTranspose', blank: true},
     { label: '替換文本', url: 'https://lzltool.com/Tools/TextReplace', blank: true },
     { label: '文本比對1', url: 'https://tonydx.github.io/frontEndDiffHelper/' , blank: true},
