0664007deb82897ffbb909f5641992f53be92fc0
primo_it <primo_it@primo-itdeiMac.local>
2026-08-16 12:14:25 +0800

    newCnavasFrameTool

--- public/floating-menu.js
+++ public/floating-menu.js
@@ -11,6 +11,7 @@
     { label: 'calc', url: '../Web-Calculator/web-calculator.html' },
     { label: 'AB表域名比對', url: '../regexDomain/abTableComparison.html' },
     { label: 'iframe Dashboard', url: '../iframe/index.html' },
+    { label: 'canvas_iframe_workspace', url: '../canvas_iframe_workspace/index.html' },
     { label: '文字找Domain', url: '../regexDomain/searchDomain.html' },
     { label: 'List 清單刪除 / 保留分行', url: '../regexDomain/listRegex.html' },
     { label: 'nsid過濾工具', url: '../regexDomain/searchnsid.html' },
